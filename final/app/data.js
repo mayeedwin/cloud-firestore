108ebf08ed863588aca62d8e34ecb8b44bd89efe
Maye Edwin <Edwin@impala.com>
2018-07-18 13:32:44 +0300

    add-files
    add-files

--- final/app/data.js
+++ final/app/data.js
@@ -5,8 +5,9 @@ docRef.set({
     next_desc: "Next ’18 is a three day global exhibition of inspiration, innovation, and education where we learn from one another how the cloud can transform how we work and power everyone’s successes.",
     recent_title: "Google IO Extended Eldoret 2018",
     recent_desc: "I/O Extended events help developers from around the world take part in the I/O experience from wherever they are. We had Talks, Hands-on sessions and I/O 18 Recap viewing",
+    timestamp: ""
 })
 
 .then(function() {
-    console.log("Document successfully updated!");
+    console.log("Document successfully created!");
 });
